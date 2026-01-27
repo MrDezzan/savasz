85dc8ad8beca0da5b480e26c73f7404a0a6c200c
MrDezzan <gromovakuuu@mail.ru>
2026-01-27 15:27:27 +0500

    dsfsd

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,18 @@ const nextConfig: NextConfig = {
   },
   // Disable powered by header
   poweredByHeader: false,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8081/api/:path*',
+      },
+      {
+        source: '/uploads/:path*',
+        destination: 'http://localhost:8081/uploads/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
